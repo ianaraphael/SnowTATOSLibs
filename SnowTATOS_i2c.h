4d2d2836f1c09e3aadb632d88d5e4966f2238ccb
ianaraphael <ian.a.raphael.th@dartmouth.edu>
2023-12-20 10:13:54 -0500

    successful server integration into barebones simb program and full simb suite

--- SnowTATOS_i2c.h
+++ SnowTATOS_i2c.h
@@ -14,8 +14,10 @@ ian.a.raphael.th@dartmouth.edu
 #include <Wire.h>
 
 #define SENSORCONTROLLER_ADDRESS 9 // Define sensor controller (SC) I2C Address
-#define MAX_PACKET_SIZE 32 // maximum dataframe size
-#define SENSORCONTROLLER_CS 6 // chip select on SC side
+#define MAX_PACKET_SIZE 32 // maximum i2c packet size
+#define SERVER_CS 11 // simb i2c chip select on server side
+// #define SERVER_CS A2 // simb i2c chip select on server side
+
 
 // collection state variable to keep track of what information we've sent to the
 // simb.
@@ -36,35 +38,54 @@ volatile uint16_t currI2cPacketSize;
 // service routine for getting a Wire data request from the SIMB. Puts the requested
 // data on the line
 void requestEvent() {
-
   // write the data to the line
   Wire.write((uint8_t *)i2cSendBuf,(uint16_t)currI2cPacketSize);
 
-  // if we're done with the SIMB transaction after this
-  if (i2cCollectionState == 0) {
+  // set withSimb false to indicate that we're no longer in a transaction
+  simbRequestFlag = false;
 
-    // set withSimb false to indicate that we're no longer in a transaction
-    simbRequestFlag = false;
-
-    // enter sleep state
-    i2cCollectionState = -1;
-
-  } else if (i2cCollectionState == 1) {
-
-    // go to data packaging state
-    i2cCollectionState = 2;
-  }
+  // enter sleep state
+  i2cCollectionState = -1;
 }
+
+// // service routine for getting a Wire data request from the SIMB. Puts the requested
+// // data on the line
+// void requestEvent() {
+//
+//   // write the data to the line
+//   Wire.write((uint8_t *)i2cSendBuf,(uint16_t)currI2cPacketSize);
+//
+//   // if we're done with the SIMB transaction after this
+//   if (i2cCollectionState == 0) {
+//
+//     // set withSimb false to indicate that we're no longer in a transaction
+//     simbRequestFlag = false;
+//
+//     // enter sleep state
+//     i2cCollectionState = -1;
+//
+//   } else if (i2cCollectionState == 1) {
+//
+//     // go to data packaging state
+//     i2cCollectionState = 2;
+//   }
+// }
 
 
 // service routine for handling chip select interrupt from SIMB
 void simbInterruptHandler(void) {
 
+  // // detach the interrupt so we don't get stuck in a loop
+  // detachInterrupt(digitalPinToInterrupt(SERVER_CS));
+  //
+  // // set simbRequestFlag true
+  // simbRequestFlag = true;
+
   // if chip select is actually low
-  if ((digitalRead(SENSORCONTROLLER_CS) == LOW) && (i2cCollectionState == -1)) {
+  if ((digitalRead(SERVER_CS) == LOW) && (i2cCollectionState == -1)) {
 
     // detach the interrupt so we don't get stuck in a loop
-    detachInterrupt(digitalPinToInterrupt(SENSORCONTROLLER_CS));
+    detachInterrupt(digitalPinToInterrupt(SERVER_CS));
 
     // set simbRequestFlag true
     simbRequestFlag = true;
@@ -75,23 +96,25 @@ void simbInterruptHandler(void) {
 // init fx for setting up i2c on sensor controller side
 void init_I2C_scSide() {
 
+  // // a little shenanigans to make sure this pin does not read low on startup
+  // pinMode(SERVER_CS,OUTPUT);
+  // digitalWrite(SERVER_CS,HIGH);
+
+  // pull up the chip select pin
+  pinMode(SERVER_CS,INPUT_PULLUP);
+
+  // delay for a moment
+  delay(10);
+
   // Initialize I2C comms as slave
   Wire.begin(SENSORCONTROLLER_ADDRESS);
 
   // Function to run when data requested from master
   Wire.onRequest(requestEvent);
 
-  // pull the chip select pin down
-  pinMode(SENSORCONTROLLER_CS,INPUT_PULLDOWN);
-
-  // pull up the chip select pin
-  pinMode(SENSORCONTROLLER_CS,INPUT_PULLUP);
-
-  // delay for a moment
-  delay(10);
-
-  // then attach an interrupt to trigger on a low pin
-  attachInterrupt(digitalPinToInterrupt(SENSORCONTROLLER_CS), simbInterruptHandler, LOW);
+  // then attach an interrupt to trigger on a falling pin
+  attachInterrupt(digitalPinToInterrupt(SERVER_CS), simbInterruptHandler, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(SERVER_CS), simbInterruptHandler, LOW);
 }
 
 // function to check if the simb has asked for data
@@ -99,16 +122,14 @@ bool simbRequestedData() {
   return simbRequestFlag;
 }
 
-// function to send data over to simb.
+
+// function to send data over to simb by i2c. MAX DATAFRAME 32 BYTES!
 void sendDataToSimb(uint8_t *simbData) {
 
   uint8_t* data = simbData;
 
-  // reset our num bytes left to send
-  int n_bytesLeftToSendSIMB = SIMB_DATASIZE;
-
-  Serial.print("SIMB requested data size. We have ");
-  Serial.print(n_bytesLeftToSendSIMB,DEC);
+  Serial.print("SIMB requested data. We have ");
+  Serial.print(SIMB_DATASIZE,DEC);
   Serial.println(" bytes to send: ");
   for (int i=0;i<SIMB_DATASIZE;i++){
     Serial.print(simbData[i],HEX);
@@ -117,78 +138,145 @@ void sendDataToSimb(uint8_t *simbData) {
   Serial.println("");
   Serial.println("Packaging data and standing by.");
 
-  // switch to i2cCollectionState 2 (data packing)
-  i2cCollectionState = 2;
+  // get the packet size
+  currI2cPacketSize = SIMB_DATASIZE;
 
-  // while we haven't sent all the data
-  while (i2cCollectionState != -1) {
-
-    // switch on the i2cCollectionState variable
-    switch(i2cCollectionState) {
-
-      // standby
-      // waiting for a data request from simb
-      case 1:
-      break; // do nothing. handled in ISR.
-
-      // data packing
-      // simb has requested data. package the data and move back to standby
-      case 2:
-
-      // if we have more data than the max packet size
-      if (n_bytesLeftToSendSIMB > MAX_PACKET_SIZE) {
-
-        // send the max amount of data
-        currI2cPacketSize = MAX_PACKET_SIZE;
-
-      } else { // otherwise
-
-        // send the data we have left
-        currI2cPacketSize = n_bytesLeftToSendSIMB;
-
-        // set collection state to 0 (pre-sleep) since we'll be done sending data after this
-        i2cCollectionState = 0;
-      }
-
-      // figure out where our current data starts and stops
-      int startIndex = SIMB_DATASIZE - n_bytesLeftToSendSIMB;
-      int endIndex = startIndex + currI2cPacketSize - 1;
-
-      // copy all of the data in
-      for (int i=startIndex; i<=endIndex; i++){
-        i2cSendBuf[i-startIndex] = data[i];
-      }
-
-      // and update n_bytesLeftToSendSIMB
-      n_bytesLeftToSendSIMB = SIMB_DATASIZE - endIndex - 1;
-
-      // add a null terminator to the buffer
-      i2cSendBuf[currI2cPacketSize] = '\0';
-
-      // preview print
-      Serial.print("SIMB has requested data. Packaging: ");
-      // Serial.println((char*) i2cSendBuf);
-      for (int i=0;i<sizeof(i2cSendBuf);i++){
-        Serial.print(i2cSendBuf[i],HEX);
-        Serial.print(" ");
-      }
-      Serial.println("");
-
-      // print how much we have left to send
-      Serial.print(n_bytesLeftToSendSIMB,DEC);
-      Serial.println(" bytes left to send.");
-
-      // if we're not supposed to go to sleep
-      if (i2cCollectionState == 2) {
-        // go back into standby, wait for request event
-        i2cCollectionState = 1;
-      }
-      break;
-    }
+  // copy all of the data in
+  for (int i=0; i<currI2cPacketSize; i++) {
+    i2cSendBuf[i] = data[i];
   }
 
+  // add a null terminator to the buffer
+  i2cSendBuf[currI2cPacketSize] = '\0';
+
+  // preview print
+  Serial.print("SIMB has requested data. Packaging: ");
+  // Serial.println((char*) i2cSendBuf);
+  for (int i=0;i<SIMB_DATASIZE;i++){
+    Serial.print(i2cSendBuf[i],HEX);
+    Serial.print(" ");
+  }
+  Serial.println("");
+
+  // go into standby mode, wait for request event
+  i2cCollectionState = 1;
+
+  // hang out while the simb has not picked up the data
+  while (i2cCollectionState != -1) {
+  }
+
+  Serial.println("SIMB collected data");
+
+  // now reset the buffer with generic mask
+  maskSimbData(simbData);
+
   // now that we're done sending data, reattach our chip select interrupt
-  attachInterrupt(digitalPinToInterrupt(SENSORCONTROLLER_CS), simbInterruptHandler, LOW);
+  attachInterrupt(digitalPinToInterrupt(SERVER_CS), simbInterruptHandler, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(SERVER_CS), simbInterruptHandler, LOW);
+
+  // if we're supposed to be asleep right now
+  if (timeToSleep) {
+    // go back to sleep
+    LowPower.standby();
+  }
 }
+
+// // function to send data over to simb.
+// void sendDataToSimb(uint8_t *simbData) {
+//
+//   uint8_t* data = simbData;
+//
+//   // reset our num bytes left to send
+//   int n_bytesLeftToSendSIMB = SIMB_DATASIZE;
+//
+//   Serial.print("SIMB requested data size. We have ");
+//   Serial.print(n_bytesLeftToSendSIMB,DEC);
+//   Serial.println(" bytes to send: ");
+//   for (int i=0;i<SIMB_DATASIZE;i++){
+//     Serial.print(simbData[i],HEX);
+//     Serial.print(" ");
+//   }
+//   Serial.println("");
+//   Serial.println("Packaging data and standing by.");
+//
+//   // switch to i2cCollectionState 2 (data packing)
+//   i2cCollectionState = 2;
+//
+//   // while we haven't sent all the data
+//   while (i2cCollectionState != -1) {
+//
+//     // switch on the i2cCollectionState variable
+//     switch(i2cCollectionState) {
+//
+//       // standby
+//       // waiting for a data request from simb
+//       case 1:
+//       break; // do nothing. handled in ISR.
+//
+//       // data packing
+//       // simb has requested data. package the data and move back to standby
+//       case 2:
+//
+//       // if we have more data than the max packet size
+//       if (n_bytesLeftToSendSIMB > MAX_PACKET_SIZE) {
+//
+//         // send the max amount of data
+//         currI2cPacketSize = MAX_PACKET_SIZE;
+//
+//       } else { // otherwise
+//
+//         // send the data we have left
+//         currI2cPacketSize = n_bytesLeftToSendSIMB;
+//
+//         // set collection state to 0 (pre-sleep) since we'll be done sending data after this
+//         i2cCollectionState = 0;
+//       }
+//
+//       // figure out where our current data starts and stops
+//       int startIndex = SIMB_DATASIZE - n_bytesLeftToSendSIMB;
+//       int endIndex = startIndex + currI2cPacketSize - 1;
+//
+//       // copy all of the data in
+//       for (int i=startIndex; i<=endIndex; i++){
+//         i2cSendBuf[i-startIndex] = data[i];
+//       }
+//
+//       // and update n_bytesLeftToSendSIMB
+//       n_bytesLeftToSendSIMB = SIMB_DATASIZE - endIndex - 1;
+//
+//       // add a null terminator to the buffer
+//       i2cSendBuf[currI2cPacketSize] = '\0';
+//
+//       // preview print
+//       Serial.print("SIMB has requested data. Packaging: ");
+//       // Serial.println((char*) i2cSendBuf);
+//       for (int i=0;i<sizeof(i2cSendBuf);i++){
+//         Serial.print(i2cSendBuf[i],HEX);
+//         Serial.print(" ");
+//       }
+//       Serial.println("");
+//
+//       // print how much we have left to send
+//       Serial.print(n_bytesLeftToSendSIMB,DEC);
+//       Serial.println(" bytes left to send.");
+//
+//       // if we're not supposed to go to sleep
+//       if (i2cCollectionState == 2) {
+//         // go back into standby, wait for request event
+//         i2cCollectionState = 1;
+//       }
+//       break;
+//     }
+//   }
+//
+//   // now that we're done sending data, reattach our chip select interrupt
+//   attachInterrupt(digitalPinToInterrupt(SERVER_CS), simbInterruptHandler, FALLING);
+//
+//   // if we're supposed to be asleep right now
+//   if (timeToSleep) {
+//     // go back to sleep
+//     LowPower.standby();
+//   }
+// }
 
 #endif
--- SnowTATOS_simbi2c.h
+++ SnowTATOS_simbi2c.h
@@ -11,6 +11,7 @@ ian.a.raphael.th@dartmouth.edu
 #ifndef SnowTATOS_simbi2c_h
 #define SnowTATOS_simbi2c_h
 
+#define FORSIMB true
 #include "SnowTATOS.h"
 #include <Wire.h>
 
@@ -47,64 +48,86 @@ void alertSensorController(){
   digitalWrite(SENSORCONTROLLER_CS,HIGH);
 }
 
-void getDataFromSensorController(uint8_t *dataBuf) {
+// updated 2023.12.18 to handle !! max 1 PACKET (32 bytes) !! of data
+void getDataFromSensorController(uint8_t dataBuf[]) {
 
-  // update how much data we're requesting
-  uint16_t n_dataToGet = SIMB_DATASIZE;
+  // flush the wire before we read anything
+  while(Wire.available()) {
+    Wire.read();
+  }
 
-  // counter variable to track where we are in the total data retrieval session
-  int i_session = 0;
+  // Serial.print("We expect this many bytes from SC: ");
+  Serial.println(SIMB_DATASIZE,DEC);
 
-  // while we haven't gotten all of the data
-  while (n_dataToGet > 0) {
+  // ask the SC to put the data on the wire
+  // Serial.print("SC put this many bytes on the wire: ");
+  uint16_t nBytesCurrPacket = Wire.requestFrom(SENSORCONTROLLER_ADDRESS,SIMB_DATASIZE);
+  Serial.println(nBytesCurrPacket,DEC);
 
-    // declare variable to keep track of the packet size we're asking for
-    int currPacketSize;
-
-    // if what's left is more than the maximum packet size
-    if (n_dataToGet > MAX_PACKET_SIZE) {
-      // request the maximum packet size
-      currPacketSize = MAX_PACKET_SIZE;
-    } else { // otherwise
-      // get what's left
-      currPacketSize = n_dataToGet;
-    }
-
-    // flush the wire before we read anything
-    while(Wire.available()) {
-      Wire.read();
-    }
-
-    Serial.print("We expect this many bytes from SC: ");
-    Serial.println(currPacketSize,DEC);
-
-    // ask the SC to put the data on the wire
-    Serial.print("SC put this many bytes on the wire: ");
-    int nBytesCurrPacket = Wire.requestFrom(SENSORCONTROLLER_ADDRESS,currPacketSize);
-    Serial.println(nBytesCurrPacket,DEC);
-
-    // delay briefly
-    delay(1000);
-
-    // counter variable to track where we are in the current data packet
-    int i_currPacket = 0;
-
-    // then, while we haven't read all of this packet
-    while (i_currPacket<currPacketSize) {
-
-      // read the next byte off the wire into the array
-      dataBuf[i_session] = Wire.read();
-
-      // and increment the counters
-      i_session++;
-      i_currPacket++;
-    }
-
-    // now decrement n_dataToGet by the amount that we read
-    n_dataToGet = n_dataToGet - currPacketSize;
+  // if we have the right number of bytes
+  if (nBytesCurrPacket < MAX_PACKET_SIZE && nBytesCurrPacket == SIMB_DATASIZE) {
+    // then read the bytes
+    Wire.readBytes(dataBuf,nBytesCurrPacket);
   }
 }
 
+// void getDataFromSensorController(uint8_t *dataBuf) {
+//
+//   // update how much data we're requesting
+//   uint16_t n_dataToGet = SIMB_DATASIZE;
+//
+//   // counter variable to track where we are in the total data retrieval session
+//   int i_session = 0;
+//
+//   // while we haven't gotten all of the data
+//   while (n_dataToGet > 0) {
+//
+//     // declare variable to keep track of the packet size we're asking for
+//     int currPacketSize;
+//
+//     // if what's left is more than the maximum packet size
+//     if (n_dataToGet > MAX_PACKET_SIZE) {
+//       // request the maximum packet size
+//       currPacketSize = MAX_PACKET_SIZE;
+//     } else { // otherwise
+//       // get what's left
+//       currPacketSize = n_dataToGet;
+//     }
+//
+//     // flush the wire before we read anything
+//     while(Wire.available()) {
+//       Wire.read();
+//     }
+//
+//     Serial.print("We expect this many bytes from SC: ");
+//     Serial.println(currPacketSize,DEC);
+//
+//     // ask the SC to put the data on the wire
+//     Serial.print("SC put this many bytes on the wire: ");
+//     int nBytesCurrPacket = Wire.requestFrom(SENSORCONTROLLER_ADDRESS,currPacketSize);
+//     Serial.println(nBytesCurrPacket,DEC);
+//
+//     // delay briefly
+//     delay(1000);
+//
+//     // counter variable to track where we are in the current data packet
+//     int i_currPacket = 0;
+//
+//     // then, while we haven't read all of this packet
+//     while (i_currPacket<currPacketSize) {
+//
+//       // read the next byte off the wire into the array
+//       dataBuf[i_session] = Wire.read();
+//
+//       // and increment the counters
+//       i_session++;
+//       i_currPacket++;
+//     }
+//
+//     // now decrement n_dataToGet by the amount that we read
+//     n_dataToGet = n_dataToGet - currPacketSize;
+//   }
+// }
 
 
 #endif
